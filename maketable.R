a5786f94ea73d1bea2773882227f4501d8332c81
David Welch <welchd@users.noreply.github.com>
2022-04-27 01:41:38 +1200

    csv to table

---
+++ maketable.R
@@ -0,0 +1,13 @@
+sig = readr::read_csv("sigs.csv")
+
+x = "<table class=\"signature_table\">"
+for (i in 1:nrow(sig)){
+  x = paste(x, "<tr>", sep = "\n")
+  x = paste(x, "\n<th class=\"name\">",sig$name[i],"</th>", sep = "")
+  x = paste(x, "\n<td class=\"title\">",sig$expertise[i],"</td>", sep = "")
+  x = paste(x, "\n<td class=\"quals\">",sig$qualifications[i],"</td>", sep = "")
+  x = paste(x, "</tr>", sep = "\n")
+}
+x = paste(x, "</table>", sep = "\n")
+
+write(x, file = "_includes/site-table2.html")
